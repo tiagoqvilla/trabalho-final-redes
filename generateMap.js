820b7b3b6b68d109beb08d4e0161bb788ad3319c
Tiago Villa <tiago_villa@hotmail.com>
2022-11-06 16:29:04 -0300

    Add mapFeatures

---
+++ generateMap.js
@@ -0,0 +1,111 @@
+const Overlap = require('overlap')
+const Couleurs = require('couleurs')
+const Box = require('cli-box')
+const rooms = require('./rooms')
+
+let generatedRooms = []
+let marksConfigs = {
+  nw: '╔',
+  n: '═',
+  ne: '╗',
+  e: '║',
+  se: '╝',
+  s: '═',
+  sw: '╚',
+  w: '║',
+}
+/**
+ * Gera as salas para o mapa
+ * @param {string} currentPlayer
+ */
+const generateRooms = (currentPlayer) => {
+  for (const [key, value] of Object.entries(rooms)) {
+    let player = value.players.includes(currentPlayer) ? 'X' : ''
+    let box = Box(
+      {
+        w: 10,
+        h: 2,
+        marks: marksConfigs,
+      },
+      Couleurs(player, '#c0392b')
+    )
+    generatedRooms.push(box)
+  }
+}
+
+/**
+ * Printa o mapa contendo a posição atual do jogador
+ * @param {string} currentPlayer
+ */
+const printMap = (currentPlayer) => {
+  generateRooms(currentPlayer)
+
+  let outerBox = Box(
+    {
+      w: 5,
+      h: 20,
+      fullscreen: true,
+      marks: marksConfigs,
+    },
+    {
+      text: Couleurs('Localização Atual do Jogador', [255, 255, 0]),
+      vAlign: 'top',
+      hAlign: 'right',
+    }
+  )
+
+  // Sala 1
+  let currentMap = Overlap({
+    who: outerBox,
+    with: generatedRooms[0],
+    where: {
+      x: 50,
+      y: 5,
+    },
+  })
+
+  // Sala 2
+  currentMap = Overlap({
+    who: currentMap,
+    with: generatedRooms[1],
+    where: {
+      x: 60,
+      y: 5,
+    },
+  })
+
+  // Sala 3
+  currentMap = Overlap({
+    who: currentMap,
+    with: generatedRooms[2],
+    where: {
+      x: 60,
+      y: 9,
+    },
+  })
+
+  // Sala 4
+  currentMap = Overlap({
+    who: currentMap,
+    with: generatedRooms[3],
+    where: {
+      x: 60,
+      y: 1,
+    },
+  })
+
+  // Sala 5
+  currentMap = Overlap({
+    who: currentMap,
+    with: generatedRooms[4],
+    where: {
+      x: 70,
+      y: 1,
+    },
+  })
+  console.log(currentMap)
+}
+
+printMap('player')
+
+module.exports = printMap
